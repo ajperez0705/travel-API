664a44505b10f8e6f943c7854247cfb02dbd5af6
Andrew <ajperez0705@gmail.com>
2021-05-16 15:58:54 -0400

    git =

--- src/js/views/popDestinationView.js
+++ src/js/views/popDestinationView.js
@@ -26,15 +26,16 @@ class PopDestinationView {
   #generateDestinationMarkup(data) {
     data.forEach((country) => {
       let markup = `
-          <a href="#${country.alphaCode}"><div id="#${country.alphaCode}" class="carousel-item destination-btn"  style="background-image: url(${country.flag});">
-                      <div class="info">
-                        <h4 class="country-name">${country.name}</h4>
-                        <div class="stars">
-                          <i class="fas fa-star"></i> 4.5
-                        </div>
-                      </div>
-                    </div>
-                    </a>
+          <a href="#${country.alphaCode}">
+            <div id="#${country.alphaCode}" class="carousel-item destination-btn"  style="background-image: url(${country.flag});">
+              <div class="info">
+                <h4 class="country-name">${country.name}</h4>
+                <div class="stars">
+                   <i class="fas fa-star"></i> 4.5
+                </div>
+              </div>
+           </div>
+          </a>aAZ
           `;
       this.#parentEl.insertAdjacentHTML("afterbegin", markup);
     });
